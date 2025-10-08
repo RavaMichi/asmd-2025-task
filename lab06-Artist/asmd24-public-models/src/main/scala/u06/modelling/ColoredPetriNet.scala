6f3a86388979af80ea0bddc9bdde7668dacb9f31
Michele Ravaioli <michele.ravaioli3@studio.unibo.it>
2025-10-08 13:10:47 +0200

    fix ColoredPetriNet

--- lab06-Artist/asmd24-public-models/src/main/scala/u06/modelling/ColoredPetriNet.scala
+++ lab06-Artist/asmd24-public-models/src/main/scala/u06/modelling/ColoredPetriNet.scala
@@ -4,7 +4,7 @@ import u06.utils.MSet
 
 object ColoredPetriNet:
   import PetriNet.*
-  case class ColoredTrn[P, C](transition: Trn[P], guard: C => Boolean, transform: Iterable[C] => C)
+  case class ColoredTrn[P, C](transition: Trn[P], guard: C => Boolean, transform: Seq[C] => C)
   type ColoredPetriNet[P, C] = Set[ColoredTrn[P, C]]
 
   given coloredTrnConversion[P, C]: Conversion[Trn[P], ColoredTrn[P, C]] with
@@ -15,15 +15,16 @@ object ColoredPetriNet:
   extension [P, C](cpn: ColoredPetriNet[P, C])
     def toSystem: System[Marking[(P, C)]] = m =>
       val mm = m.asList.toMap
+      def isInhibited(t: Trn[P]): Boolean = mm.keys.exists(t.inh.asList.contains)
+      def guardCheckFailed(guard: C => Boolean): Boolean = !mm.values.exists(guard)
       cpn
         .map { case ColoredTrn(transition, guard, transform) => (transition, guard, transform) }
-        .map:
-          (tr, g, f) =>
-            if mm.keys.exists(tr.inh.asList.contains) then // inhibition
-              MSet()
-            else if mm.values.exists(g) then // color guard
-              MSet()
-            else
-              lazy val cond = mm.filter((p, c) => tr.cond.asList.contains(p))
-              lazy val out = tr.eff.asList.map(p => (p, f(cond.values)))
-              MSet.ofList(mm.removedAll(cond.keys).toList ++ out)
+        .map((tr, g, f) =>
+          val cond = mm.filter((p, c) => tr.cond.asList.contains(p))
+          if isInhibited(tr) || guardCheckFailed(g) || cond.isEmpty then
+            List.empty
+          else
+            val eff = tr.eff.asList.map(p => (p, f(cond.values.toSeq)))
+            mm.removedAll(cond.keys).toList ++ eff
+        ).filterNot(_.isEmpty)
+        .map(MSet.ofList)
