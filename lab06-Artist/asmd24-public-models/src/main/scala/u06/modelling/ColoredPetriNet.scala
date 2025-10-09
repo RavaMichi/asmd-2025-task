8dc400bf39ac6660fdaf00149ea22329c95007e0
Michele Ravaioli <michele.ravaioli3@studio.unibo.it>
2025-10-09 12:17:34 +0200

    add precondition color calculator

--- lab06-Artist/asmd24-public-models/src/main/scala/u06/modelling/ColoredPetriNet.scala
+++ lab06-Artist/asmd24-public-models/src/main/scala/u06/modelling/ColoredPetriNet.scala
@@ -7,10 +7,30 @@ object ColoredPetriNet:
   case class ColoredTrn[P, C](transition: Trn[P], guard: C => Boolean, transform: Seq[C] => C)
   type ColoredPetriNet[P, C] = Set[ColoredTrn[P, C]]
 
-  given coloredTrnConversion[P, C]: Conversion[Trn[P], ColoredTrn[P, C]] with
-    override def apply(x: Trn[P]): ColoredTrn[P, C] = ColoredTrn(x, c => true, _.head)
-
   def apply[P, C](transitions: ColoredTrn[P, C]*): ColoredPetriNet[P, C] = transitions.toSet
+
+  // helper function
+  def combinations[A](list: Set[A], size: Int): Set[Set[A]] = size match
+    case 0 => Set()
+    case 1 => list.map(Set(_))
+    case s =>
+      for
+        x <- list
+        comb <- combinations(list.filter(x != _), s - 1)
+      yield comb + x
+
+  def computeColorsOfPrecondition[P, C](state: List[(P, C)], cond: List[P]): Set[MSet[(P, C)]] =
+    state
+      .flatMap(x => cond.find(_ == x._1).map(c => x))
+      .flatMap(x =>
+        val newCond = cond diff Seq(x._1)
+        if newCond.isEmpty then
+          Set(MSet(x))
+        else
+          val newState = state diff Seq(x)
+          computeColorsOfPrecondition(newState, newCond)
+            .map(_ union MSet(x))
+      ).toSet
 
   extension [P, C](cpn: ColoredPetriNet[P, C])
     def toSystem: System[Marking[(P, C)]] = m =>
@@ -29,10 +49,23 @@ object ColoredPetriNet:
         ).filterNot(_.isEmpty)
         .map(MSet.ofList)
 
-  extension [P, C](ctr: ColoredTrn[P, C])
-    infix def when(guard: C => Boolean): ColoredTrn[P, C] =
+  given coloredTrnConversion[P, C]: Conversion[Trn[P], ColoredTrn[P, C]] with
+    override def apply(x: Trn[P]): ColoredTrn[P, C] = ColoredTrn(x, c => true, _.head)
+
+
+
+  enum ColorAttribute[C]:
+    case Guard(guard: C => Boolean)
+    case OnTransition(tr: Seq[C] => C)
+
+  import ColorAttribute.*
+  def when[C](guard: PartialFunction[C, Boolean]): Guard[C] = Guard(c => guard.applyOrElse(c, _ => false))
+  infix def onTransition[C](tr: Seq[C] => C): OnTransition[C] = OnTransition(tr)
+
+  extension [P](tr: Trn[P])
+    infix def |[C](cc: ColorAttribute[C]*): ColoredTrn[P, C] =
       ColoredTrn(
-        ctr.transition,
-        c => ctr.guard(c) && guard(c),
-        ctr.transform
+        tr,
+        c => cc.collect { case x: Guard[C] => x }.forall(_.guard(c)),
+        cc.collectFirst { case x: OnTransition[C] => x }.map(_.tr).getOrElse(_.head)
       )
--- lab06-Artist/asmd24-public-models/src/test/scala/u06/modelling/CPNSpec.scala
+++ lab06-Artist/asmd24-public-models/src/test/scala/u06/modelling/CPNSpec.scala
@@ -18,6 +18,39 @@ class CPNSpec extends AnyFunSuite:
   import ColoredPetriNet.*
   import Place.*
   import Color.*
+
+  test("testing combinations"):
+//    extension [P, C](cpn: ColoredPetriNet[P, C])
+//      def asSystem: System[Marking[(P, C)]] = m =>
+//        def places: MSet[(P, C)] => MSet[P] = x => MSet.ofList(x.asList.map(_._1))
+//        def colors: MSet[(P, C)] => MSet[C] = x => MSet.ofList(x.asList.map(_._2))
+//        for
+//          ColoredTrn(Trn(cond, eff, inh), guard, f) <- cpn   // get any transition
+//          if inh disjoined places(m)
+//          token <- cond.asList
+//          (_, color) <- m.asList.find(_ == token)
+//          if guard(color) // color guard
+//
+//        yield ???
+
+    def computeColorsOfPrecondition[P, C](state: List[(P, C)], cond: List[P]): Set[MSet[(P, C)]] =
+      state
+        .flatMap(x => cond.find(_ == x._1).map(c => x))
+        .flatMap(x =>
+          val newCond = cond diff Seq(x._1)
+          if newCond.isEmpty then
+            Set(MSet(x))
+          else
+            val newState = state diff Seq(x)
+            computeColorsOfPrecondition(newState, newCond)
+              .map(_ union MSet(x))
+        ).toSet
+
+    val state = MSet(P1 -> Red(0), P1 -> Blue(""), P2 -> Red(2), P2 -> Red(1), P3 -> Blue(""))
+    val cond = MSet(P2, P1, P3)
+    val x = computeColorsOfPrecondition(state.asList, cond.asList)
+    println(x)
+
   
   test("CPN should be able to move token without changing their color"):
     
@@ -33,7 +66,7 @@ class CPNSpec extends AnyFunSuite:
 
   test("CPN should enable transitions when guard check passed"):
     val testCPN = ColoredPetriNet[Place, Color](
-      MSet(P1) ~~> MSet(P2) when (c => c.isInstanceOf[Red]),
+      MSet(P1) ~~> MSet(P2) | when { case Red(_) => true },
       MSet(P2) ~~> MSet(P3),
     ).toSystem
 
@@ -41,3 +74,19 @@ class CPNSpec extends AnyFunSuite:
 
     testCPN.paths(MSet(P1 -> Red(0), P1 -> Blue("")), 3).toSet should be:
       Set(expected)
+
+  test("CPN should update the color when firing a transition"):
+
+    val testCPN = ColoredPetriNet[Place, Color](
+      MSet(P1) ~~> MSet(P2) | onTransition {
+        case Seq(Red(i)) => Red(i + 1)
+        case Seq(Blue(s)) => Blue(s + "+")
+      },
+    ).toSystem
+
+    val expected1 = List(MSet(P1 -> Red(0), P1 -> Blue("")), MSet(P2 -> Red(1), P1 -> Blue("")), MSet(P2 -> Red(1), P2 -> Blue("+")))
+    val expected2 = List(MSet(P1 -> Red(0), P1 -> Blue("")), MSet(P1 -> Red(0), P2 -> Blue("+")), MSet(P2 -> Red(1), P2 -> Blue("+")))
+
+    testCPN.paths(MSet(P1 -> Red(0), P1 -> Blue("")), 3).toSet should be:
+      Set(expected1, expected2)
+
